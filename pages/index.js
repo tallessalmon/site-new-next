8f9965196a15697c30ac989ab79f5474fae7a3a3
Talles Salmon <tallessalmon@hotmail.com>
2020-12-17 17:26:53 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-      <div className={styles.bgimage}>
+      <div className={styles.container,styles.bgimage}>
       <Head>
         <title>Next JS TESTE</title>
         <link rel="icon" href="/talles.ico" />
